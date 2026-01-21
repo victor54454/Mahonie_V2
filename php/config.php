727db40034ce87967d9d3d5dab57ffefe7d7438f
viktor54454 <victor.mercier@e-cdp.com>
2026-01-21 10:14:43 +0100

    mise anjour du config pour cacher les valeurs

--- php/config.php
+++ php/config.php
@@ -5,13 +5,13 @@
 
 // SMTP Gmail configuration
 define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_USER', 'merciervictor4@gmail.com');  // TON email Gmail
-define('SMTP_PASS', 'hpvtnmpcfjenjbqh');   // Le mdp d'app que tu as créé
+define('SMTP_USER', '');  // TON email Gmail
+define('SMTP_PASS', '');   // Le mdp d'app que tu as créé
 define('SMTP_PORT', 587);
 define('SMTP_SECURE', 'tls');
 
 // Email où tu reçois les notifications
-define('ADMIN_EMAIL', 'merciervictor4@gmail.com');
+define('ADMIN_EMAIL', '');
 
 // Timezone²
 date_default_timezone_set('Europe/Paris');
